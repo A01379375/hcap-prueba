75fcaa5d33a9710815c1a1cf37eef3af13a20eee
A01379375 <thebloodprince@DESKTOP-Q6FAUQ6.localdomain>
2020-10-28 14:44:14 -0600

    Segunda modificacion de miconvolucion.py

--- miconvolucion.py
+++ miconvolucion.py
@@ -20,12 +20,15 @@ Filtro = [[1, 0, 2], [5, 0, 9], [6, 2, 1]]
 A = np.array(Matriz1)
 B = np.array(Filtro)
 
-C = np.zeros((2, 2))
-
-print(A)
-
-print(A[1][0])
-
-print(C)
-
+print('Ejemplo 1')
 print(convolucion(A, B))
+
+print('\nEjemplo 2')
+
+Matriz2 = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [0, 0, 1, 16, 17, 18] ,[0, 1, 0, 7, 23, 24], [1, 7, 6, 5, 4, 3]]
+Filtro2 = [[1, 1, 1], [0, 0, 0], [2, 10, 3]]
+
+A2 = np.array(Matriz2)
+B2 = np.array(Filtro2)
+
+print(convolucion(A2, B2))
